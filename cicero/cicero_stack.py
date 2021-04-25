f34d4516395f62d43f0064bf7723eb3323425609
Cameron Braverman <31460379+bravc@users.noreply.github.com>
2021-04-25 17:20:26 -0400

    Frontend Video Upload and DyanmoDB metadata storage (#4)
    * Video upload from frontend.
    
    * Add dynamodb to store metadata.

--- cicero/cicero_stack.py
+++ cicero/cicero_stack.py
@@ -5,7 +5,10 @@ from aws_cdk import (
     aws_iam as _iam,
     aws_s3_notifications,
     aws_s3_deployment as s3_deployment,
+    aws_apigateway as apigateway,
+    aws_dynamodb as dynamodb,
 )
+
 
 class CiceroStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
@@ -28,9 +31,7 @@ class CiceroStack(cdk.Stack):
                 _iam.ManagedPolicy.from_aws_managed_policy_name(
                     "AmazonPollyFullAccess"
                 ),
-                _iam.ManagedPolicy.from_aws_managed_policy_name(      
-                    "AmazonSESFullAccess"
-                ),
+                _iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSESFullAccess"),
             ],
         )
 
@@ -50,6 +51,25 @@ class CiceroStack(cdk.Stack):
             ),
         )
 
+        api = apigateway.RestApi(
+            self,
+            "upload-api",
+            rest_api_name="Cicero Service",
+            default_cors_preflight_options=apigateway.CorsOptions(
+                allow_origins=apigateway.Cors.ALL_ORIGINS,
+                allow_methods=apigateway.Cors.ALL_METHODS,
+            ),
+        )
+
+        # Dynamo DB
+        video_table = dynamodb.Table(
+            self,
+            "VideoTable",
+            partition_key=dynamodb.Attribute(
+                name="filename", type=dynamodb.AttributeType.STRING
+            ),
+        )
+
         s3_deployment.BucketDeployment(
             self,
             "DeployWebsite",
@@ -63,6 +83,19 @@ class CiceroStack(cdk.Stack):
             "VideoUploadBucket",
             auto_delete_objects=True,
             removal_policy=cdk.RemovalPolicy.DESTROY,
+            block_public_access=s3.BlockPublicAccess(
+                restrict_public_buckets=False,
+                ignore_public_acls=False,
+                block_public_policy=False,
+                block_public_acls=False,
+            ),
+            cors=[
+                s3.CorsRule(
+                    allowed_origins=["*"],
+                    allowed_headers=["*"],
+                    allowed_methods=[s3.HttpMethods.PUT],
+                )
+            ],
         )
         transcribe_bucket = s3.Bucket(
             self,
@@ -93,6 +126,20 @@ class CiceroStack(cdk.Stack):
         )
 
         # Initial Video Upload and Transcription
+        video_upload_lambda = _lambda.Function(
+            self,
+            "video_upload_lambda_function",
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            handler="video_upload.main",
+            timeout=cdk.Duration.seconds(30),
+            code=_lambda.Code.from_asset("./cicero/lambda"),
+            role=lambda_role,
+            environment={
+                "BUCKET": video_upload_bucket.bucket_name,
+                "TABLE": video_table.table_name,
+            },
+        )
+        video_table.grant_write_data(video_upload_lambda)
         transcribe_lambda = _lambda.Function(
             self,
             "lambda_function",
@@ -120,7 +167,7 @@ class CiceroStack(cdk.Stack):
         # Replace sender@example.com with your "From" address.
         # This address MUST be verified with Amazon SES.
 
-        # Replace recipient@example.com with a "To" address. If your account 
+        # Replace recipient@example.com with a "To" address. If your account
         # is still in the sandbox, this address must be verified.
         sns_lambda = _lambda.Function(
             self,
@@ -130,16 +177,27 @@ class CiceroStack(cdk.Stack):
             timeout=cdk.Duration.seconds(30),
             code=_lambda.Code.from_asset("./cicero/lambda"),
             role=lambda_role,
-            environment={"RECIEVER": "recipient@example.com", "SENDER": "Sender Name <sender@example.com>"},
+            environment={
+                "RECIEVER": "recipient@example.com",
+                "SENDER": "Sender Name <sender@example.com>",
+            },
         )
 
         # create s3 notification for lambda function
         notification = aws_s3_notifications.LambdaDestination(transcribe_lambda)
 
         # create s3 notification for lambda function
-        new_translation_notification = aws_s3_notifications.LambdaDestination(polly_lambda)
+        new_translation_notification = aws_s3_notifications.LambdaDestination(
+            polly_lambda
+        )
         finished_video_notification = aws_s3_notifications.LambdaDestination(sns_lambda)
 
+        # API Gateway add route
+        video_upload_integration = apigateway.LambdaIntegration(
+            video_upload_lambda,
+            request_templates={"application/json": '{ "statusCode": "200" }'},
+        )
+        api.root.add_method("POST", video_upload_integration)
         # assign notification for the s3 event type
         video_upload_bucket.add_event_notification(
             s3.EventType.OBJECT_CREATED, notification
---
+++ cicero/lambda/video_upload.py
@@ -0,0 +1,33 @@
+import boto3
+import os
+import json
+
+dynamodb = boto3.resource("dynamodb")
+
+
+def main(event, context):
+    _json = json.loads(event["body"])
+    filename = _json["filename"]
+    lang = _json["language"]
+    email = _json["email"]
+    bucket = os.getenv("BUCKET")
+    table_name = os.getenv("TABLE")
+    table = dynamodb.Table(table_name)
+
+    url = boto3.client("s3").generate_presigned_url(
+        ClientMethod="put_object",
+        Params={"Bucket": bucket, "Key": filename},
+        ExpiresIn=3600,
+    )
+
+    table.put_item(Item={"filename": filename, "language": lang, "email": email})
+
+    return {
+        "statusCode": 200,
+        "body": url,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+    }
