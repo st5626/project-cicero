bf0e28221c3d16ad4544fdc56e5e6471bff384a1
Su Thit Thazin <st5626@g.rit.edu>
2021-04-27 14:04:20 -0400

    reverting merge

--- cicero/lambda/video_upload.py
+++ cicero/lambda/video_upload.py
@@ -1,7 +1,6 @@
 import boto3
 import os
 import json
-import uuid
 
 dynamodb = boto3.resource("dynamodb")
 
@@ -9,28 +8,19 @@ dynamodb = boto3.resource("dynamodb")
 def main(event, context):
     _json = json.loads(event["body"])
     filename = _json["filename"]
-    input_lang = _json["input_language"]
-    target_lang = _json["target_language"]
-
+    lang = _json["language"]
     email = _json["email"]
     bucket = os.getenv("BUCKET")
     table_name = os.getenv("TABLE")
     table = dynamodb.Table(table_name)
-    key = str(uuid.uuid4())
+
     url = boto3.client("s3").generate_presigned_url(
         ClientMethod="put_object",
-        Params={"Bucket": bucket, "Key": key},
+        Params={"Bucket": bucket, "Key": filename},
         ExpiresIn=3600,
     )
 
-    table.put_item(Item={
-        "uuid": key,
-        "filename": filename, 
-        "input_language": input_lang,
-        "target_language": target_lang,
-        "email": email,
-        }
-    )
+    table.put_item(Item={"filename": filename, "language": lang, "email": email})
 
     return {
         "statusCode": 200,
