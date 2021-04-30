35e66c4b83464df1a1465cd0bea42737a4004e88
Su Thit Thazin <st5626@g.rit.edu>
2021-04-29 23:26:40 -0400

    more cleanup

--- cicero/lambda/polly.py
+++ cicero/lambda/polly.py
@@ -5,9 +5,6 @@ import time
 
 
 def main(event, context):
-
-    # TARGET_LANGUAGE = "de"
-
     polly = boto3.client("polly")
     s3 = boto3.resource("s3")
     dynamodb = boto3.resource("dynamodb")
@@ -58,7 +55,6 @@ def main(event, context):
         OutputS3BucketName=output_bucket,
         Text=textToSynthesize,
         VoiceId=getVoiceId(TARGET_LANGUAGE, polly)
-        # LanguageCode='en-AU',                     # 'arb'|'cmn-CN'|'cy-GB'|'da-DK'|'de-DE'|'en-AU'|'en-GB'|'en-GB-WLS'|'en-IN'|'en-US'|'es-ES'|'es-MX'|'es-US'|'fr-CA'|'fr-FR'|'is-IS'|'it-IT'|'ja-JP'|'hi-IN'|'ko-KR'|'nb-NO'|'nl-NL'|'pl-PL'|'pt-BR'|'pt-PT'|'ro-RO'|'ru-RU'|'sv-SE'|'tr-TR'
     )
 
     while synthesis_task["SynthesisTask"]["TaskStatus"] != "completed":
